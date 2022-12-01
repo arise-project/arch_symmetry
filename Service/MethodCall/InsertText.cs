f808537c81b9ca82909311040365c57866e3916d
Evgeny.Pirogov <Evgeny.Pirogov@aspose.com>
2022-12-01 10:31:58 +0200

    disable empty snippet

--- Service/MethodCall/InsertText.cs
+++ Service/MethodCall/InsertText.cs
@@ -7,11 +7,18 @@ namespace arch_sync.Service.MethodCall
     {
         public void Insert(string file, string marker, string snippet)
         {
-            var text = File.ReadAllText(file);
-            int start = text.IndexOf(marker);
-            var beg = text.Substring(0, start);
-            var end = text.Substring(start + marker.Length);
-            File.WriteAllText(file, beg + snippet + Environment.NewLine + end);
+            if(!string.IsNullOrWhiteSpace(snippet))
+            {
+                var text = File.ReadAllText(file);
+                int start = text.IndexOf(marker);
+                var beg = text.Substring(0, start);
+                var end = text.Substring(start + marker.Length);
+                File.WriteAllText(file, beg + snippet + Environment.NewLine + end);
+            }
+            else
+            {
+                Console.WriteLine("error: snippet is empty");
+            }
         }
     }
 }
